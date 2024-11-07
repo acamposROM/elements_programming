5b87e8f7a569781d8207fe71bbe9d5eb1b7efd2b
acamposROM <acampos341@gmail.com>
2024-11-07 14:47:11 -0500

    figured out how to render network graphs in iterm in the gas station code. formatting updates

--- dynamic_programming/levenshtein_dist.py
+++ dynamic_programming/levenshtein_dist.py
@@ -1,19 +1,20 @@
 def pp_2d_word(s, t, matrix, blank_head=True):
-    print(' ', end='  ') # padding for 2d array printing
+    print(" ", end="  ")  # padding for 2d array printing
     if blank_head:
-        print('_', end='  ')
+        print("_", end="  ")
     for c in t:
-        print(c, end= '  ')
+        print(c, end="  ")
     print()
     for i, r in enumerate(matrix):
         if not blank_head:
             i += 1
         if i == 0:
-            print('_', end=' ')
+            print("_", end=" ")
         else:
-            print(s[i-1], end=' ')
+            print(s[i - 1], end=" ")
         print(r)
     print()
+
 
 # How many edits does it take to transform t to s?
 def levenshtein_dist(s, t):
@@ -27,17 +28,19 @@ def levenshtein_dist(s, t):
     # How do we DP this?
     s_len = len(s) + 1
     t_len = len(t) + 1
-    place_holder = s_len + t_len 
+    place_holder = s_len + t_len
     dp = [[place_holder] * t_len for _ in range(s_len)]
     dp[0][0] = 0
     pp_2d_word(s, t, dp)
-    
+
     def recurse(p_s, p_t):
         if p_s < 0 or p_t < 0:
-            return place_holder 
+            return place_holder
         if dp[p_s][p_t] != place_holder:
             return dp[p_s][p_t]
-        dp[p_s][p_t] = min(recurse(p_s - 1, p_t - 1), recurse(p_s, p_t - 1), recurse(p_s - 1, p_t))
+        dp[p_s][p_t] = min(
+            recurse(p_s - 1, p_t - 1), recurse(p_s, p_t - 1), recurse(p_s - 1, p_t)
+        )
         curr_s_char = s[p_s - 1] if p_s - 1 >= 0 else ""
         curr_t_char = t[p_t - 1] if p_t - 1 >= 0 else ""
         if curr_s_char != curr_t_char:
@@ -48,7 +51,8 @@ def levenshtein_dist(s, t):
     pp_2d_word(s, t, dp)
     return dp[-1][-1]
 
-def lcs_str(s: str, t:str) -> str:
+
+def lcs_str(s: str, t: str) -> str:
     ans = ""
     s_len = len(s) + 1
     t_len = len(t) + 1
@@ -57,12 +61,17 @@ def lcs_str(s: str, t:str) -> str:
     dp[0][0] = 0
     for i in range(1, s_len):
         for j in range(1, t_len):
-            if s[i-1] == t[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-                
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > len(ans):
+                    ans += s[i - 1]
             else:
-                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+    pp_2d_word(s, t, dp)
     return ans
+
+
 def longest_common_subsequence(s, t):
     s_len = len(s) + 1
     t_len = len(t) + 1
@@ -71,10 +80,15 @@ def longest_common_subsequence(s, t):
     dp[0][0] = 0
     for i in range(1, s_len):
         for j in range(1, t_len):
-            dp[i][j] = dp[i-1][j-1] + 1 if s[i-1] == t[j-1] else max(dp[i][j-1], dp[i-1][j])
+            dp[i][j] = (
+                dp[i - 1][j - 1] + 1
+                if s[i - 1] == t[j - 1]
+                else max(dp[i][j - 1], dp[i - 1][j])
+            )
 
     pp_2d_word(s, t, dp)
     return dp[-1][-1]
+
 
 def num_of_edits_string_to_palindrom(s):
     s_len = len(s)
@@ -87,18 +101,19 @@ def num_of_edits_string_to_palindrom(s):
                 dp[i][j] = 1
             elif s[i] != s[j]:
                 if i + 1 > index_len:
-                    dp[i][j] = dp[i][j-1]
+                    dp[i][j] = dp[i][j - 1]
                 elif j - 1 < 0:
-                    dp[i][j] = dp[i+1][j]
+                    dp[i][j] = dp[i + 1][j]
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
             else:
-                dp[i][j] = 2 + dp[i+1][j-1]
+                dp[i][j] = 2 + dp[i + 1][j - 1]
             j += 1
             i += 1
     pp_2d_word(s, s, dp, blank_head=False)
-    print(f'Number of deletions needed: {s_len - dp[0][index_len]}\n')
+    print(f"Number of deletions needed: {s_len - dp[0][index_len]}\n")
     return s_len - dp[0][index_len]
+
 
 # Apparently we do not need to care abou the case where both substrings
 # have the same curr char and we can simplify the loop.
@@ -107,18 +122,19 @@ def chatgpt_is_interleave(s1: str, s2: str, t: str) -> bool:
         return False
     s1_len = len(s1) + 1
     s2_len = len(s2) + 1
-    
+
     dp = [[False] * s2_len for _ in range(s1_len)]
-    dp[0][0] = True 
+    dp[0][0] = True
 
     for i in range(s1_len):
         for j in range(s2_len):
-            if i > 0 and s1[i-1] == t[i+j-1]:
-                dp[i][j] = dp[i][j] or dp[i-1][j]
-            if j > 0 and s2[j-1] == t[i + j - 1]:
+            if i > 0 and s1[i - 1] == t[i + j - 1]:
+                dp[i][j] = dp[i][j] or dp[i - 1][j]
+            if j > 0 and s2[j - 1] == t[i + j - 1]:
                 dp[i][j] = dp[i][j] or dp[i][j - 1]
 
     return dp[s1_len - 1][s2_len - 1]
+
 
 def valid_interleaving_string(t, s1, s2):
     s1_len = len(s1) + 1
@@ -136,12 +152,17 @@ def valid_interleaving_string(t, s1, s2):
         curr_c = t[n]
         # When both strings match the current letter, we go right first. if we pop a pivot, we check if we already
         # visited the right cell with the last condition in the if
-        if i+1 <= (s2_len - 1) and curr_c == s2[i] and j+1 <= (s1_len - 1) and curr_c == s1[j]:
+        if (
+            i + 1 <= (s2_len - 1)
+            and curr_c == s2[i]
+            and j + 1 <= (s1_len - 1)
+            and curr_c == s1[j]
+        ):
             pivot_points.append((i, j, n))
             j += 1
-        elif i+1 <= (s2_len - 1) and curr_c == s2[i]:
+        elif i + 1 <= (s2_len - 1) and curr_c == s2[i]:
             i += 1
-        elif j+1 <= (s1_len - 1) and curr_c == s1[j]:
+        elif j + 1 <= (s1_len - 1) and curr_c == s1[j]:
             j += 1
         else:
             if pivot_points:
@@ -158,10 +179,12 @@ def valid_interleaving_string(t, s1, s2):
     print(True)
     return True
 
+
 # num_of_edits_string_to_palindrom("abdbca")
 # num_of_edits_string_to_palindrom("racecar")
 # num_of_edits_string_to_palindrom("character")
-#valid_interleaving_string("gatacta", "gtaa", "atc")
-#print(chatgpt_is_interleave("gatacta", "gtaa", "atc"))
+# valid_interleaving_string("gatacta", "gtaa", "atc")
+# print(chatgpt_is_interleave("gatacta", "gtaa", "atc"))
 
 print(longest_common_subsequence("cat", "cra"))
+print(lcs_str("cata", "craia"))
--- gas_station/gas_station.py
+++ gas_station/gas_station.py
@@ -1,4 +1,4 @@
-'''
+"""
     We have a vehicle with tank capactiy of U.
     U is the distance the vehcile may travel on a full tank of gas.
     Vertex V is a gas station, where gas can be purchased for c(v), cost of gas per mile
@@ -9,11 +9,20 @@
 
     Variation: we can only stop to get gas at most N times.
     Variation: Cheapest route that visits a set of p locations in a specified order
-'''
+"""
+
+import os
+
 import graphviz
-import tempfile
+import matplotlib
+
+matplotlib.use("module://itermplot")
+os.environ["ITERMPLOT_LINES"] = "10"
+from io import BytesIO
+
 import matplotlib.pyplot as plt
 from PIL import Image
+
 
 # Nodes will have lists for edges.
 # An edge will be a tuple (node, cost to get to node)
@@ -22,54 +31,60 @@ class GasStation:
         self.name = name
         self.cost = cost
 
-class GasGraph:
+
+class Graph:
     def __init__(self) -> None:
         self.nodes = {}
         self.edges = {}
-        self.dot = graphviz.Digraph() 
-     
+        self.dot = graphviz.Digraph(
+            graph_attr={
+                "rankdir": "LR",
+            }
+        )
+
     def add_node(self, a: GasStation):
         self.nodes[a.name] = a
 
     def add_edge(self, a: str, b: str, weight: int):
         self.edges[(a, b)] = weight
-   
+
     def get_node_edges(self, a: str) -> dict:
-        return {k:v for k, v  in self.edges.items() if k[0] == a}
-    
+        return {k: v for k, v in self.edges.items() if k[0] == a}
+
     def init_graphviz(self) -> None:
         for n in self.nodes.values():
-            self.dot.node(n.name, f'{n.name} Cost: {n.cost}')
-        
+            self.dot.node(n.name, f"{n.name} Cost: {n.cost:.2f}")
+
         for k, v in self.edges.items():
             a, b = k
-            self.dot.edge(str(a), str(b), label=f'{v}')
+            self.dot.edge(str(a), str(b), label=f"{v}")
 
     def visualize_graph(self) -> None:
-        with tempfile.NamedTemporaryFile(suffix=".png") as temp_file:
-            self.dot.render(filename=temp_file.name, format="png")
-            img = Image.open(temp_file.name + ".png")
+        img_data = self.dot.pipe(format="png")
+        img = Image.open(BytesIO(img_data))
+        img = img.crop(img.getbbox())
+        plt.figure(figsize=(16, 6), dpi=300)
+        plt.imshow(img)
+        plt.axis("off")
+        plt.show()
 
-            plt.imshow(img)
-            plt.axis("off")
-            plt.show()
     def __repr__(self) -> str:
         str_builder = []
         # List all nodes
         for n in self.nodes.values():
-            str_builder.append(f'Node: {n.name} Cost: {n.cost}')
-        
-        #List all edges
-        for k,v in self.edges.items():
+            str_builder.append(f"Node: {n.name} Cost: {n.cost:.2f}")
+
+        # List all edges
+        for k, v in self.edges.items():
             a, b = k
-            str_builder.append(f'Edge ({a},{b}) Weight: {v}')
+            str_builder.append(f"Edge ({a},{b}) Weight: {v}")
 
         return "\n".join(str_builder)
-            
 
-a = GasStation('A', 1.00)
-b = GasStation('B', 2.00)
-G = GasGraph()
+
+a = GasStation("A", 1.00)
+b = GasStation("B", 2.00)
+G = Graph()
 G.add_node(a)
 G.add_node(b)
 G.add_edge(a.name, b.name, 10)
